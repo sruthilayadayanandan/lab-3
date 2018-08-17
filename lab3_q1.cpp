e663382462cfab900ac2d6a6857f32233f0be929
sruthilayadayanandan <42140072+sruthilayadayanandan@users.noreply.github.com>
2018-08-17 11:17:50 +0530

    Add files via upload

---
+++ lab3_q1.cpp
@@ -0,0 +1,18 @@
+//first include library
+#include<iostream>
+using namespace std;
+//write the function
+int main()
+{
+//declaring the variables
+int a;
+float b;
+char c;
+bool d;
+//print the results
+cout << "size of int is " << sizeof(a) << endl;
+cout << "size of float is " << sizeof(b) << endl;
+cout << "size of char is " << sizeof(c) << endl;
+cout << "size of bool is " << sizeof(d) << endl; 
+return 6;
+}
---
+++ lab3_q2.cpp
@@ -0,0 +1,22 @@
+// include the library
+#include <iostream>
+using namespace std;
+//write the function
+int main ()
+{
+//declare the variables
+int a;
+float b;
+int c;
+//assigning the values
+a=3;
+b=3.7;
+c=6;
+//printing the result
+cout << "the sum is " << a+b << endl;
+cout << "the difference is " << a-b << endl;
+cout << "the product is " << a*c << endl;
+cout << "the quotient is " << b/a << endl;
+cout << "the modulus is " << c%a << endl;
+}
+ 
